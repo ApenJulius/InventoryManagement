1ef120d0e3ce38e3fbf2528ce783ebb59fbf0551
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-07 23:19:44 +0100

    Add Account and Main components

---
+++ frontend/src/pages/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+import Inventory from '../../components/Inventory/Inventory';
+
+function Main() {
+    return (
+        <div className="Main" style={{ display:'flex' }}>
+            <Inventory />
+
+        </div>
+    );
+}
+
+export default Main;
