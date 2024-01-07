0d939458ed629b05f3ac730860a780b1e233d11f
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-07 23:20:53 +0100

    Update import path for Main component

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Main from './pages/Main';
+import Main from './pages/Main/Main';
 
 test('renders learn react link', () => {
     render(<App />);
