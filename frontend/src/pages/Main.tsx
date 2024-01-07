811c813de380b3bbff848fec1e7ee30160782f09
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-07 16:06:30 +0100

    Add Inventory component to Main page

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Main.css';
 import Inventory from '../components/Inventory/Inventory';
+
 function Main() {
     return (
         <div className="Main" style={{ display:'flex' }}>
             <Inventory />
+
         </div>
     );
 }
