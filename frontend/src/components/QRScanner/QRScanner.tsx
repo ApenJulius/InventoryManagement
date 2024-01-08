84c3e9c4e723774d224b3a84a0fc0e83e6d31e11
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-08 02:54:09 +0100

    Using advanced settings now

--- frontend/src/components/QRScanner/QRScanner.tsx
+++ frontend/src/components/QRScanner/QRScanner.tsx
@@ -1,40 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import './QRScanner.css';
-import { Html5QrcodeScanner } from 'html5-qrcode';
+import { Html5Qrcode } from 'html5-qrcode';
 
 
 const QRScanner: React.FC = () => {
-    const [scanResult, setScanResult] = useState(null);
-
-    useEffect(() => {
-        const qrScanner = new Html5QrcodeScanner('reader', { 
-            fps: 10, 
-            qrbox: { 
-                width: 250, 
-                height: 250 
-            } 
-        },
-        false);
-        qrScanner.render(success, error);
-
-        function success(result) {
-            qrScanner.clear();
-            setScanResult(result);
-            console.log('success');
-        }
-        function error(err: any) {
-            console.error(err);
-        }
+/*     const [scanResult, setScanResult] = useState(null);
+    const qrScanner = new Html5Qrcode('reader');
 
 
-    }, []);
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    };
+    const qrCodeErrorCallback = (decodedText, decodedResult) => {
+    };
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+     */
+    // If you want to prefer front camera
+    //qrScanner.start({ facingMode: 'user' }, config, qrCodeSuccessCallback, qrCodeErrorCallback);
+    
 
     return ( 
-        <div>
-            {scanResult
-                ? <p>RESULT: {scanResult}</p>
-                : <div id="reader">Scan a QR code</div>
-            }
+        <div className='QRScan-container'>
+            
         </div>
     );
 };
