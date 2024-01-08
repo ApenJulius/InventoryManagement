97626c3c23a6d55bcac9611c6a666dbafd62a7ef
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-08 14:43:11 +0100

    Add Footer component

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+
+const Footer: React.FC = () => {
+    
+    
+    return ( 
+        <footer>
+            THIS IS FOOOOTER
+        </footer>
+    );
+};
+
+export default Footer;
