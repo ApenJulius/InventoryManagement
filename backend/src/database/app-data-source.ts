8f12bc96d122d6edeea721acb968834b7663b83c
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-09 22:24:01 +0100

    Logging got in the way

--- backend/src/database/app-data-source.ts
+++ backend/src/database/app-data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 export const Database = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
-    logging: true,
+    logging: false,
     entities: ['dist/database/models/*.js'],
     //migrations: ['./migrations/*.js'],
     subscribers: [],
