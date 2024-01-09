621a9863b0ce10cb1dee80614fd8d6874f0f1eca
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-09 14:21:59 +0100

    Moved types to frontend as well

---
+++ frontend/src/types.ts
@@ -0,0 +1,12 @@
+export enum UserRole {
+    ADMIN = 'ADMIN',
+    MANAGER = 'MANAGER',
+    USER = 'USER'
+}
+
+export enum ProductStatus{
+    AVAILABLE = 'AVAILABLE',
+    BORROWED = 'BORROWED',
+    BROKEN = 'BROKEN',
+    LOST = 'LOST'
+}
