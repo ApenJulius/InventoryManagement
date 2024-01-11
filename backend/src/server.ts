786e28ca44c031f2100ac3d6f8bdb7bcb4616f72
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-11 16:50:08 +0100

    Generate new on api call

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,9 @@ import 'reflect-metadata';
 import { Database } from './database/app-data-source';
 import express, { Application } from 'express';
 import { Products } from './database/models/Products';
-import { validateQRCode } from './utils/middleware';
+import { validateAddProduct, validateQRCode } from './utils/middleware';
 import cors from 'cors';
+import { ProductStatus } from './types';
 
 Database.initialize().then(() => {
     console.log('Database initialized');
@@ -35,11 +36,37 @@ app.post('/qrScan', async (req, res) => {
     // TODO: Implement this endpoint with returning he product in pop up, or question of creating
 
 });
-
-app.post('/updateProduct', async (req, res) => {
-    // TODO: Implement this endpoint with updating the product
+function generateRandomString(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) 
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    
+    return result;
+}
+function generateBulkProducts(productName: string, productAmount: number) {
+    const products = [];
+    
+    for (let i = 0; i < productAmount; i++) {
+        products.push({ 
+            name: productName, 
+            identifier: generateRandomString(25), 
+            status: ProductStatus.AVAILABLE  }); 
+    }
+    return products;
+}
+app.post('/products/add', async (req, res) => {
+    const productName: string = req.body.name;
+    const productAmount: number = req.body.amount;
+    if (!validateAddProduct(productName, productAmount)) {
+        res.status(400).send('Could not validate product');
+        return;
+    }
+    const products = generateBulkProducts(productName, productAmount);
+    console.log(products);
+    await Database.createQueryBuilder().insert().into(Products).values(products).execute();
+    return res.status(200).send('Products added');
 });
-
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on port ${process.env.PORT || 3000}`);
