41538f2324eee53d6328978568750f429ae0e326
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-11 16:49:09 +0100

    AddProductModal now hides properly

--- frontend/src/components/SideTab/SideTab.tsx
+++ frontend/src/components/SideTab/SideTab.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './SideTab.css';
 import QRScanner from '../QRScanner/QRScanner';
+import AddProductModal from '../AddProductModal/AddProductModal';
 
 interface IUser {
     name: string;
@@ -10,9 +11,12 @@ interface IUser {
 
 const SideTab: React.FC<IUser> = ({ name, role }) => {
     const [showQR, setShowQR] = React.useState(false);
-
+    const [showAddProduct, setShowAddProduct] = React.useState(false);
     const toggleQR = () => {
         setShowQR(!showQR);
+    };
+    const toggleAddProduct = () => {
+        setShowAddProduct(!showAddProduct);
     };
     // Add your component logic here
     return ( 
@@ -34,11 +38,20 @@ const SideTab: React.FC<IUser> = ({ name, role }) => {
                         Inventory
                     </button>
                     {
+                        showAddProduct ?
+                            <AddProductModal onOutsideClick={toggleAddProduct}/>
+                            : null
+
+                    }
+                    {
                         showQR ?
                             <QRScanner onOutsideClick={toggleQR}/>
                             : null
                     }
                 </div>
+                <button onClick={toggleAddProduct}>
+                        Add Product
+                </button>
                 <button onClick={toggleQR} className='sidebar-button qr-button'>
                 </button>
             </div>
