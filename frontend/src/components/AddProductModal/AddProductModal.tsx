662fe4b9136a76d58d034ec092b7e1c615e733b1
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-01-11 16:51:37 +0100

    Allows for adding products to the db

---
+++ frontend/src/components/AddProductModal/AddProductModal.tsx
@@ -0,0 +1,60 @@
+
+import Veil from '../Veil/Veil';
+import './AddProductModal.css';
+interface IProductModule {
+    onOutsideClick?: () => void;
+}
+
+
+const AddProductModal: React.FC<IProductModule> = ({ onOutsideClick }) => {
+
+    const handleOutsideClick = async () => {
+        if (onOutsideClick)
+            onOutsideClick();
+    };
+    
+    const addProducts = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/products/add`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                name:e.target.name.value,
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                amount:e.target.amount.value
+            })
+        }).then(res => res.json()).then(data => {
+            console.log(data);
+        }).catch(err => {
+            console.log(err);
+        });
+    };
+
+
+    return ( 
+        <Veil onOutsideClick={handleOutsideClick}>
+            <div className="add-product-container">
+                <form style={{ display:'flex', flexDirection:'column' }} onSubmit={addProducts}>
+                    <div className='label-input-div'>
+                        <label>Name </label>
+                        <input type="text" placeholder="Product Name" name="name"/>
+                       
+                    </div>
+                    <div className="label-input-div">
+                        <label>Amount </label>
+                        <input type="number" placeholder='Product Amount' inputMode='numeric' name="amount"/>
+                    </div>
+                    <button>Create</button>
+                    <span style={{ fontSize:'XX-small' }}>*You can edit after adding</span>
+                </form>
+            </div>
+        </Veil>
+    );
+};
+
+export default AddProductModal;
